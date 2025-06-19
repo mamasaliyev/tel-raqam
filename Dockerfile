2dab54f35c2e614abf0dd8acd1ed44f93ed8a9d7
Urolbek2002 <zokirovurolbek2@gmail.com>
2025-06-19 17:25:09 +0500

    docker

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,28 @@
-# Python imageni tanlash
-FROM python:3.13
+FROM python:3.10
+
+ENV PYTHONDONTWRITEBYTECODE=1
+ENV PYTHONUNBUFFERED=1
 
 # Ishchi katalog
 WORKDIR /app
 
-# Talablarni o‘rnatish
-COPY requirements.txt .
-RUN pip install --no-cache-dir -r requirements.txt
+# OS kutubxonalar (dlib uchun kerak bo‘lsa)
+RUN apt-get update && apt-get install -y \
+    build-essential cmake libopenblas-dev liblapack-dev libx11-dev \
+    libgtk-3-dev libboost-all-dev libpq-dev && \
+    rm -rf /var/lib/apt/lists/*
 
-# Django kodini joylash
+# Talablar
+COPY requirements.txt .
+RUN pip install --upgrade pip && pip install --no-cache-dir -r requirements.txt
+
+# Loyihani nusxalash
 COPY . .
 
-# Django uchun port
+# Static/media kataloglar (Docker volume overwrite qiladi baribir)
+RUN mkdir -p /app/staticfiles /app/mediafiles
+
+# Port ochish
 EXPOSE 8000
 
-# Serverni ishga tushurish
-CMD ["gunicorn", "config.wsgi:application", "--bind", "0.0.0.0:8000"]
+# Run qilish — docker-compose orqali beriladi
--- config/settings.py
+++ config/settings.py
@@ -77,11 +77,32 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB', 'phone'),
+        'USER': os.environ.get('POSTGRES_USER', 'user_phone'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'password_phone'),
+        'HOST': os.environ.get('POSTGRES_HOST', 'db'),
+        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
     }
 }
 
